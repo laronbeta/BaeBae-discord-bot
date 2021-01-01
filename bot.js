f4b98cb585f744c032f8e6d9105678065126ccf3
Ronaldi Chandra Karuna <chandraronaldi@yandex.com>
2021-01-01 14:14:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,40 +51,6 @@ function randomRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-client.on("guildCreate", guild => {
-  let channelID;
-  let channels = guild.channels;
-  channelLoop: for (let c of channels) {
-    let channelType = c[1].type;
-    if (channelType === "text") {
-      channelID = c[0];
-      break channelLoop;
-    }
-  }
-  let channel = client.channels.get(guild.systemChannelID || channelID);
-  const embed = new RichEmbed()
-    .setTitle("Hai, terima kasih telah mengundangku ke server ini.")
-    .setColor(0xd17eff);
-  channel.send(embed);
-});
-
-client.on("guildCreate", guild => {
-  let channelID;
-  let channels = guild.channels;
-  channelLoop: for (let c of channels) {
-    let channelType = c[1].type;
-    if (channelType === "text") {
-      channelID = c[0];
-      break channelLoop;
-    }
-  }
-  let channel = client.channels.get(guild.systemChannelID || channelID);
-  const embed = new RichEmbed()
-    .setTitle("Ketik .help untuk menampilkan daftar perintah.")
-    .setColor(0xd17eff);
-  channel.send(embed);
-});
-
 
 client.on("message", message => {
   let cont = message.content.slice(prefix.length).split(" ");
