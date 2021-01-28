f6e836e774ac26c98bb2ae8369c26a9b5e807778
Ronaldi Chandra Karuna <48817684+ronaldichdr@users.noreply.github.com>
2021-01-28 20:12:52 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ client.on("message", message => {
     if(!message.guild.member(client.user).hasPermission('MANAGE_ROLES')) return;
     var colors = ['#8585ff','#fff681','#a073fd','#fd73b9'];
     for(let i = 0; i<= colors.length;i++){
-    var role = message.guild.roles.find(role => role.name === "rolename");
+    var role = message.guild.roles.find(role => role.name === "colorized");
     setInterval(() => {
         role.edit({
             color: colors[i]
