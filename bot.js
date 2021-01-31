ef3129651c78557dceea9a4df1490e8390d0eb63
Ronaldi Chandra Karuna <48817684+ronaldichdr@users.noreply.github.com>
2021-01-31 09:17:01 +0700

    change my uname

--- bot.js
+++ bot.js
@@ -380,7 +380,7 @@ if(message.content === prefix+'test'){
             .addField(".corona", "Untuk menampilkan info statistik corona di indonesia.")
             .addField(".shalat", "Untuk menampilkan jadwal shalat di Tangerang.")
             .addField(".help", "Untuk menampilkan pesan ini.")
-            .setFooter("Developed by ronaldichdr);
+            .setFooter("Developed by ronaldichdr");
         message.channel.send(embed);
     }
 });
